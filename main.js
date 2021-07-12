8a9d142df71dd9b79de6a56dd2e4e15152268894
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2021-07-12 12:00:55 +0300

    minor fixes

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
 // get DOM elements
-let ham = document.querySelector('.ham');
 let ul = document.querySelector('ul');
-let one = document.querySelector('.ham-bar-one');
-let two = document.querySelector('.ham-bar-two');
+let hamMenu = document.querySelector('.ham-menu');
+let hamMenuLayers = document.querySelectorAll('.ham-layer');
+let hamMenuCircles = document.querySelectorAll('.ham-circle');
 let nav = document.querySelector('.nav');
 let links = document.querySelectorAll('.nav-links');
-let heroTextCovers = document.querySelectorAll('.hero-txt-cover')
 
 // event listeners
-ham.addEventListener('click', menuToggler);
+hamMenu.addEventListener('click', menuToggler);
 links.forEach( link => {
     link.addEventListener('click', menuToggler);
 })
 
 // menu toggler
 function menuToggler(){
-    ul.classList.toggle('active');
-    one.classList.toggle('one');
-    two.classList.toggle('two');
+    hamMenuLayers.forEach(elem => {
+        elem.classList.toggle('active');
+    });
+    hamMenuCircles.forEach(elem => {
+        elem.classList.toggle('active');
+    });
     nav.classList.toggle('nav-active')
+    ul.classList.toggle('active');
 }
 
